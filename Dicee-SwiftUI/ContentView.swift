2d4d46cf77e578fe089dee8704f0ff0a85eaa9f1
smease12 <scmease93@gmail.com>
2024-09-09 12:58:58 -0400

    Initial commit

---
+++ Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by Serena  on 9/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Image("background")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Serena  on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
